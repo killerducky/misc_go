3e136cdb4f772fd7731b7d1ae32fa32048cfafc6
unknown <aolsen@.austin.nextio>
2009-05-28 16:30:34 -0500

    Make a Ratings object, use some nicer Python stuff. Output is almost identical

--- histogram/hist.py
+++ histogram/hist.py
@@ -4,6 +4,7 @@
 # Outputs a google chart API url 
 
 import math
+import sys
 
 # AGA form skips all numbers (-1.0, 1)
 # Linear form adds 2 to all kyu numbers to fill this gap
@@ -57,23 +58,54 @@ import math
 # 100   10.12   10.50     7.89     8.09
 # 
 
+class Rating:
+   def __init__(self, rating, style="aga"):
+      if (style == "aga"):
+         if rating < 0: rating -= 0.000001   # special case kyus so the borders work?  super hack!
+         self.rating = self.aga2lin(rating)
+      elif (style == "lin"):
+         self.rating = rating
+      else: raise
+   def __str__(self, style="dankyu", precision=-1):
+      if (style == "dankyu"):
+         if precision == -1: precision = 0
+         tmp = self.lin2aga(self.rating)
+         if (tmp < 0): return "%0.*f%s" % (precision, -tmp, "k")
+         else:         return "%0.*f%s" % (precision,  tmp, "d")
+      elif (style == "aga"):
+         if precision == -1: precision = 2
+         return "%0.*f" % (precision, self.lin2aga(self.rating))
+      elif (style == "lin"):
+         if precision == -1: precision = 2
+         return "%0.*f" % (precision, self.rating)
+      else: 
+         raise
+   def lin2aga(self, rating_lin):
+      if (rating_lin < 1): return rating_lin-2
+      else:                return rating_lin
+   def aga2lin(self, rating_aga):
+      if (rating_aga < 0): return rating_aga+2
+      else:                return rating_aga
+   def linear_weakest(self):
+      return math.floor(self.rating)
+      #if (self.rating >= 1): return math.floor(self.rating)
+      #else:                  return math.floor(self.rating+1)
 
 percent = [1,2,5,10,20,30,40,50,60,70,80,90,95,98,99,99.5]
-KGS_linear = [-22.26, -20.30, -17.20, -13.36, -9.26, -6.94, -5.18, -3.65, -2.19, -0.73, 0.72, 2.52, 3.88, 5.29, 6.09, 7.20]
-AGA_linear = [-32.61,-30.58,-25.69,-21.47,-16.54,-11.91,-7.90,-5.10,-2.59,0.15,2.10,4.71,6.12,7.41,8.15,8.70]
-EGF_linear = [-19.00,-19.00,-18.47,-15.44,-10.47,-8.00,-6.13,-4.43,-2.91,-1.16,0.39,2.17,3.39,4.60,5.36,6.04]
+KGS_ratings = [Rating(x, "lin") for x in [-22.26, -20.30, -17.20, -13.36, -9.26, -6.94, -5.18, -3.65, -2.19, -0.73, 0.72, 2.52, 3.88, 5.29, 6.09, 7.20]]
+AGA_ratings = [Rating(x, "lin") for x in [-32.61,-30.58,-25.69,-21.47,-16.54,-11.91,-7.90,-5.10,-2.59,0.15,2.10,4.71,6.12,7.41,8.15,8.70]]
+EGF_ratings = [Rating(x, "lin") for x in [-19.00,-19.00,-18.47,-15.44,-10.47,-8.00,-6.13,-4.43,-2.91,-1.16,0.39,2.17,3.39,4.60,5.36,6.04]]
 USCF = [444,531,663,793,964,1122,1269,1411,1538,1667,1807,1990,2124,2265,2357,2470]
 
-
 # labels are not in linear
-labels_AGA = [-25,-24,-23,-22,-21,-20,-19,-18,-17,-16,-15,-14,-13,-12,-11,-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7,8,9]
+labels_dankyu = [Rating(x, "aga") for x in [-25,-24,-23,-22,-21,-20,-19,-18,-17,-16,-15,-14,-13,-12,-11,-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7,8,9]]
 labels_USCF = [500, 1000, 1500, 2000, 2500]
 
-minrate = -23.99  # weakest 25k
-maxrate = 9.99    # strongest 9d
 
 #def scale(x, min, max):
 def scale(x):
+   minrate = -23.99  # weakest 25k
+   maxrate = 9.99    # strongest 9d
    return math.floor((x-minrate)/(maxrate-minrate)*10000)/100   # should do the rounding in the display
 
 def scale_USCF(x):
@@ -81,20 +113,8 @@ def scale_USCF(x):
    maxrate = 2500.0
    return math.floor((x-minrate)/(maxrate-minrate)*10000)/100   # should do the rounding in the display
 
-def linear_weakest(x):
-   if (x<0):
-      return x+1
-   if (x>0):
-      return x
-
-labels_weakest = map(linear_weakest, labels_AGA)
-labels_scale = map(scale, labels_weakest)
-
 labels_USCF_scale = map(scale_USCF, labels_USCF)
 
-KGS_scale = map(scale, KGS_linear)
-AGA_scale = map(scale, AGA_linear)
-EGF_scale = map(scale, EGF_linear)
 USCF_scale = map(scale_USCF, USCF)
 
 red     = "ff0000"
@@ -111,18 +131,18 @@ url.append("&chdl=AGA|EGF|KGS|USCF")
 url.append("&chxt=x,y,t")
 url.append("&chxtc=0,-900|1,-900|2,0")
 url.append("&chco=%s,%s,%s,%s" % (red, green, blue, magenta))
-url.append("&chd=t:" + str.join(",", map(str, AGA_scale)))
-url.append("|"       + str.join(",", map(str, percent)))
-url.append("|"       + str.join(",", map(str, EGF_scale)))
-url.append("|"       + str.join(",", map(str, percent)))
-url.append("|"       + str.join(",", map(str, KGS_scale)))
-url.append("|"       + str.join(",", map(str, percent)))
-url.append("|"       + str.join(",", map(str, USCF_scale)))
-url.append("|"       + str.join(",", map(str, percent)))
-url.append("&chxl=0:|" + str.join("|", map(str, labels_AGA)))
-url.append("|2:|" + str.join("|", map(str, labels_USCF)))
-url.append("&chxp=0," + str.join(",", map(str, labels_scale)))
-url.append("|2," + str.join(",", map(str, labels_USCF_scale)))
+url.append("&chd=t:"   + ",".join([str(scale(x.rating)) for x in AGA_ratings]))
+url.append("|"         + ",".join(map(str, percent)))
+url.append("|"         + ",".join([str(scale(x.rating)) for x in EGF_ratings]))
+url.append("|"         + ",".join(map(str, percent)))
+url.append("|"         + ",".join([str(scale(x.rating)) for x in KGS_ratings]))
+url.append("|"         + ",".join(map(str, percent)))
+url.append("|"         + ",".join(map(str, USCF_scale)))
+url.append("|"         + ",".join(map(str, percent)))
+url.append("&chxl=0:|" + "|".join([x.__str__("aga", 0) for x in labels_dankyu]))
+url.append("|2:|"      + "|".join(map(str, labels_USCF)))
+url.append("&chxp=0,"  + ",".join([str(scale(x.linear_weakest())) for x in labels_dankyu]))
+url.append("|2,"       + ",".join(map(str, labels_USCF_scale)))
 
 print str.join("\n", url)
 
