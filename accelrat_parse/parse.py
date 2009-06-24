0b23472ae88c3e3f9ff5eecc4c2ce3054f98d37e
killerducky <aolsen@.austin.nextio>
2009-06-24 17:08:23 -0500

    Handle byes, add curr vs initial rating code

--- accelrat_parse/parse.py
+++ accelrat_parse/parse.py
@@ -31,14 +31,14 @@ class Game:
    def my_opp(self, me):
       if self.white == me: return self.black 
       else:                return self.white
-   def get_my_rating(self, me):
-      if self.white == me: return self.get_white_rating()
-      else:                return self.get_black_rating()
-   def get_white_rating(self, use_accel_rating=1):
-      if use_accel_rating: return self.accel_white_rating
+   def get_my_rating(self, me, use_accel_rat=1):
+      if self.white == me: return self.get_white_rating(use_accel_rat)
+      else:                return self.get_black_rating(use_accel_rat)
+   def get_white_rating(self, use_accel_rat=1):
+      if use_accel_rat: return self.accel_white_rating
       else: return self.white.rating
-   def get_black_rating(self, use_accel_rating=1):
-      if use_accel_rating: return self.accel_black_rating
+   def get_black_rating(self, use_accel_rat=1):
+      if use_accel_rat: return self.accel_black_rating
       else: return self.black.rating
    def handi_advantage(self):
       if self.handicap == 0:
@@ -49,19 +49,19 @@ class Game:
       else:
          handi_advantage = self.handicap - 0.5
       return handi_advantage
-   def rating_advantage(self, use_accel_rating=1):
-      if (self.get_white_rating() * self.get_black_rating()) < 0 : 
-         return self.get_white_rating() - self.get_black_rating() - 2   # compensate for dan/kyu border
+   def rating_advantage(self, use_accel_rat=1):
+      if (self.get_white_rating(use_accel_rat) * self.get_black_rating(use_accel_rat)) < 0 : 
+         return self.get_white_rating(use_accel_rat) - self.get_black_rating(use_accel_rat) - 2   # compensate for dan/kyu border
       else:
-         return self.get_white_rating() - self.get_black_rating()
+         return self.get_white_rating(use_accel_rat) - self.get_black_rating(use_accel_rat)
    def my_result(self, me):
       if self.white == me: return self.winner=="W"
       else:                return self.winner=="B"
-   def white_advantage(self):
-      return self.rating_advantage() - self.handi_advantage()
-   def my_advantage(self, me):
-      if self.white == me: return self.white_advantage()
-      else:                return -self.white_advantage()
+   def white_advantage(self, use_accel_rat=1):
+      return self.rating_advantage(use_accel_rat) - self.handi_advantage()
+   def my_advantage(self, me, use_accel_rat=1):
+      if self.white == me: return self.white_advantage(use_accel_rat)
+      else:                return -self.white_advantage(use_accel_rat)
 
 # 1RATSWP.txt format:
 #\t2.7\t\tOlsen, Andy
@@ -87,8 +87,8 @@ def main():
       if len(line) == 0: continue         # if nothing left, continue
       if re.search('"', line):
          agaid  = re.search('^(\d+)', line).group(1)
-         name   = re.search('Name="(.*?)"', line).group(1)
-         rating = re.search('Rating="(.*?)"', line).group(1)
+         name   = re.search('name="(.*?)"', line, re.I).group(1)
+         rating = re.search('rating="(.*?)"', line, re.I).group(1)
       else:
          agaid, name, rating = line.split("\t")[:3]
          #agaid, name, rating = m[:3]
@@ -114,19 +114,23 @@ def main():
       f = open(str(round)+".tde")
       for line in f:
          line = line.rstrip()
-         line = re.sub("##.*", "", line)
-         if len(line) == 0: continue
-         m = re.split("\s*[#:]\s*", line)
-         white, black, winner, handicap, komi = re.split("\s+", m[0])
-         handicap = int(handicap)
-         komi = int(komi)
-         white_rating = re.search("[-\d\.]+", m[1]).group()
-         black_rating = re.search("[-\d\.]+", m[2]).group()
-         white_rating = float(white_rating)
-         black_rating = float(black_rating)
-         game = Game(white, black, winner, handicap, komi, white_rating, black_rating)
-         players_byaga[white].round.append(game)
-         players_byaga[black].round.append(game)
+         m = re.search("## BYE (\d+)", line)
+         if m:
+            players_byaga[m.group(1)].round.append(None)
+         else:
+            line = re.sub("##.*", "", line)
+            if len(line) == 0: continue
+            m = re.split("\s*[#:]\s*", line)
+            white, black, winner, handicap, komi = re.split("\s+", m[0])
+            handicap = int(handicap)
+            komi = int(komi)
+            white_rating = re.search("[-\d]+\.[\d]+", m[1]).group()
+            black_rating = re.search("[-\d]+\.[\d]+", m[2]).group()
+            white_rating = float(white_rating)
+            black_rating = float(black_rating)
+            game = Game(white, black, winner, handicap, komi, white_rating, black_rating)
+            players_byaga[white].round.append(game)
+            players_byaga[black].round.append(game)
       f.close
       round += 1
    total_rounds = round - 1
@@ -137,29 +141,36 @@ def main():
    print "<table border=\"1\">"
    print "<tr><td>agaid</td><td>name</td><td>rating</td>",
    for x in range(1, total_rounds+1): print tdwrap(x)    
-   #print tdwrap(x) for x in range(1, total_rounds+1)     # why can't I do a comprehension here?
    print tdwrap("final")
    print "</tr>"
    players = players_byaga.values();
    players.sort(key=operator.attrgetter("rating"), reverse=True)
    for player in players:
-      #for tmp in [player.agaid, player.name, player.rating, player.truerating]: print tdwrap(tmp)
       print tdwrap(player.agaid), tdwrap(player.name)
       print tdwrap("enter %0.2f" % (player.rating) + "<br>" + "true %0.2f" % (player.truerating))
       for curr_round in range(1, total_rounds+1):
          game = player.round[curr_round-1]
          print "<td>",
-         assert game.get_my_rating(player) == player.new_rating[curr_round-1]
-         print "my_curr_rating", game.get_my_rating(player), "<br>",
-         print "opp ", game.my_opp(player).name, "<br>",
-         print "opp_rating %0.2f" % (game.my_opp(player).rating), "<br>",
-         print "opp_curr_rating %0.2f" % (game.get_my_rating(game.my_opp(player))), "<br>",
-         print "win?", game.my_result(player), "<br>",
-         print "handi", "%0.2f" % (game.handi_advantage()), "<br>",
-         print "rat_diff", "%0.2f" % (game.rating_advantage()), "<br>",
-         print "my_adv", "%0.2f" %(game.my_advantage(player)), "<br>",
+         if game == None:
+            print "BYE <br>",
+            print "my_curr_rating", player.new_rating[curr_round-1], "<br>",
+         else:
+            assert game.get_my_rating(player) == player.new_rating[curr_round-1]
+            print "my_curr_rating", game.get_my_rating(player), "<br>",
+            print "opp ", game.my_opp(player).name, "<br>",
+            print "opp_rating %0.2f" % (game.my_opp(player).rating), "<br>",
+            print "opp_curr_rating %0.2f" % (game.get_my_rating(game.my_opp(player))), "<br>",
+            print "win?", game.my_result(player), "<br>",
+            print "handi", "%0.2f" % (game.handi_advantage()), "<br>",
+            print "curr_rat_diff", "%0.2f" % (game.rating_advantage()), "<br>",
+            print "curr_my_adv", "%0.2f" %(game.my_advantage(player)), "<br>",
+            print "init_rat_diff", "%0.2f" % (game.rating_advantage(0)), "<br>",
+            print "init_my_adv", "%0.2f" %(game.my_advantage(player, 0)), "<br>",
          print "</td>"
-      print tdwrap(player.new_rating[total_rounds])
+      if len(player.new_rating)>total_rounds:
+         print tdwrap(player.new_rating[total_rounds])
+      else:
+         print tdwrap("XXX")
       print "</tr>"
    print "</table>"
 
