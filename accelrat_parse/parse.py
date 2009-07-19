116ff234304dd54d7a0f56491e8f9624b85a93b1
unknown <Owner@.(none)>
2009-07-19 17:22:29 -0500

    Handle a few more player drop type cases

--- accelrat_parse/parse.py
+++ accelrat_parse/parse.py
@@ -6,18 +6,19 @@ import os
 import math
 import operator
 
-# hack globals
+# globals
 players_byaga = {}
 players_byname = {}   # I'm sure there is some other awesome way, hack
 
+VERBOSE = 1
 
 class Player:
    def __init__(self, agaid, name, rating):
       self.agaid      = int(agaid)
       self.name       = name
       self.rating     = float(rating)
-      self.round      = []
-      self.new_rating = []
+      self.round      = {}
+      self.new_rating = {}
 
 class Game:
    def __init__(self, white, black, winner, handicap, komi, white_rating, black_rating):
@@ -42,9 +43,9 @@ class Game:
       else: return self.black.rating
    def handi_advantage(self):
       if self.handicap == 0:
-         if   self.komi ==  5: handi_advantage = 0.0
-         elif self.komi ==  0: handi_advantage = 0.5
-         elif self.komi == -5: handi_advantage = 1.0
+         if   self.komi >  0: handi_advantage = 0.0
+         elif self.komi == 0: handi_advantage = 0.5
+         elif self.komi <  0: handi_advantage = 1.0
          else: sys.exit("invalid self.komi:" + str(self.komi))
       else:
          handi_advantage = self.handicap - 0.5
@@ -65,7 +66,7 @@ class Game:
 
 # 1RATSWP.txt format:
 #\t2.7\t\tOlsen, Andy
-def parse_ratswp(filename):
+def parse_ratswp(round, filename):
    f = open(filename)
    for line in f:
       #line = line.lstrip(line.rstrip())   # why can't I do this in one step?
@@ -73,7 +74,7 @@ def parse_ratswp(filename):
       line = line.rstrip()
       (rating, win_loss_record, name) = re.split("\t", line)
       rating = float(rating)
-      players_byname[name].new_rating.append(rating)
+      players_byname[name].new_rating[round] = rating
    f.close
 
 def tdwrap(td):
@@ -97,17 +98,18 @@ def main():
       players_byname[name] = player
    f.close
 
-   f = open("TRUERATS.tde")
-   for line in f:
-      m = re.split("\s+|=", line)
-      agaid = m[0]
-      truerating = float(m[2])
-      players_byaga[agaid].truerating = truerating
-   f.close
+   #f = open("TRUERATS.tde")
+   #for line in f:
+   #   m = re.split("\s+|=", line)
+   #   agaid = m[0]
+   #   truerating = float(m[2])
+   #   players_byaga[agaid].truerating = truerating
+   #f.close
 
    round = 1
    while (os.path.exists(str(round)+".tde")):
-      parse_ratswp(str(round)+"RATSWP.txt")
+      filename = str(round)+"RATSWP.txt"
+      parse_ratswp(round, filename)
 
       # X.tde format:
       # 7998 4408 B 0 5 # Olsen, Andy 2.6 : Blake, Ken -1.0
@@ -116,7 +118,7 @@ def main():
          line = line.rstrip()
          m = re.search("## BYE (\d+)", line)
          if m:
-            players_byaga[m.group(1)].round.append(None)
+            players_byaga[m.group(1)].round[round] = None
          else:
             line = re.sub("##.*", "", line)
             if len(line) == 0: continue
@@ -129,14 +131,14 @@ def main():
             white_rating = float(white_rating)
             black_rating = float(black_rating)
             game = Game(white, black, winner, handicap, komi, white_rating, black_rating)
-            players_byaga[white].round.append(game)
-            players_byaga[black].round.append(game)
+            players_byaga[white].round[round] = game
+            players_byaga[black].round[round] = game
       f.close
       round += 1
    total_rounds = round - 1
 
    # Parse ratings after final round
-   parse_ratswp("RATSWP.txt")
+   parse_ratswp(total_rounds+1, "RATSWP.txt")
 
    print "<table border=\"1\">"
    print "<tr><td>agaid</td><td>name</td><td>rating</td>",
@@ -147,28 +149,44 @@ def main():
    players.sort(key=operator.attrgetter("rating"), reverse=True)
    for player in players:
       print tdwrap(player.agaid), tdwrap(player.name)
-      print tdwrap("enter %0.2f" % (player.rating) + "<br>" + "true %0.2f" % (player.truerating))
+      print "<td>",
+      print "enter %0.2f" % (player.rating) + "<br>"
+      if total_rounds+1 in player.new_rating:
+         print "final", player.new_rating[total_rounds+1]
+      print "</td>"
       for curr_round in range(1, total_rounds+1):
-         game = player.round[curr_round-1]
          print "<td>",
-         if game == None:
-            print "BYE <br>",
-            print "my_curr_rating", player.new_rating[curr_round-1], "<br>",
+         if curr_round in player.round and player.round[curr_round] != None:
+            game = player.round[curr_round]
+            # I think this only broke because a player entered and never played
+            #assert game.get_my_rating(player) == player.new_rating[curr_round]
+            if VERBOSE:
+               print "my_curr_rating", game.get_my_rating(player), "<br>",
+               print "opp ", game.my_opp(player).name, "<br>",
+               print "opp_rating %0.2f" % (game.my_opp(player).rating), "<br>",
+               print "opp_curr_rating %0.2f" % (game.get_my_rating(game.my_opp(player))), "<br>",
+               print "win?", game.my_result(player), "<br>",
+               print "handi", "%0.2f" % (game.handi_advantage()), "<br>",
+               print "curr_rat_diff", "%0.2f" % (game.rating_advantage()), "<br>",
+               print "curr_my_adv", "%0.2f" %(game.my_advantage(player)), "<br>",
+               print "init_rat_diff", "%0.2f" % (game.rating_advantage(0)), "<br>",
+               print "init_my_adv", "%0.2f" %(game.my_advantage(player, 0)), "<br>",
+            else:
+               print "opp ", game.my_opp(player).name, "<br>",
+               if game.handi_advantage() < 1.5:
+                  print "handi=", "%0.1f" % (game.handi_advantage()), "<br>",
+               else:
+                  print "handi=", "%0.0f" % (game.handi_advantage()+0.5), "<br>",
+               print ("lose", "win")[game.my_result(player)], "<br>",
          else:
-            assert game.get_my_rating(player) == player.new_rating[curr_round-1]
-            print "my_curr_rating", game.get_my_rating(player), "<br>",
-            print "opp ", game.my_opp(player).name, "<br>",
-            print "opp_rating %0.2f" % (game.my_opp(player).rating), "<br>",
-            print "opp_curr_rating %0.2f" % (game.get_my_rating(game.my_opp(player))), "<br>",
-            print "win?", game.my_result(player), "<br>",
-            print "handi", "%0.2f" % (game.handi_advantage()), "<br>",
-            print "curr_rat_diff", "%0.2f" % (game.rating_advantage()), "<br>",
-            print "curr_my_adv", "%0.2f" %(game.my_advantage(player)), "<br>",
-            print "init_rat_diff", "%0.2f" % (game.rating_advantage(0)), "<br>",
-            print "init_my_adv", "%0.2f" %(game.my_advantage(player, 0)), "<br>",
+            print "BYE <br>",
+            if curr_round in player.new_rating:
+               print "my_curr_rating", player.new_rating[curr_round], "<br>",
+            else:
+               print "my_curr_rating", "XXX", "<br>",
          print "</td>"
-      if len(player.new_rating)>total_rounds:
-         print tdwrap(player.new_rating[total_rounds])
+      if total_rounds+1 in player.new_rating:
+         print tdwrap(player.new_rating[total_rounds+1])
       else:
          print tdwrap("XXX")
       print "</tr>"
